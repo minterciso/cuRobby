c4d7991380233359ce646e060d045cf270dc1bba
Mateus Interciso <minterciso@gmail.com>
2019-01-23 16:13:49 -0200

    * Added the GA files

---
+++ src/ga.cu
@@ -0,0 +1,8 @@
+/*
+ * ga.cu
+ *
+ *  Created on: 23/01/2019
+ *      Author: minterciso
+ */
+
+
---
+++ src/ga.h
@@ -0,0 +1,15 @@
+/*
+ * ga.h
+ *
+ *  Created on: 23/01/2019
+ *      Author: minterciso
+ */
+
+#ifndef GA_H_
+#define GA_H_
+
+
+
+
+
+#endif /* GA_H_ */
